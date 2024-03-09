47a430805246b0445f3ede73e42e0bb3dd19666a
rod409 <radoyeh@gmail.com>
2024-03-08 16:18:56 -0800

    add 4MP test

---
+++ config/test_4MP.py
@@ -0,0 +1,32 @@
+model = dict(
+    image_size = (1440, 2560),
+    feat_size = [(180, 320), (90, 160), (45, 80), (23, 40), (21, 38), (19, 36)],
+    steps = [8, 16, 32, 64, 100, 300],
+    scales = [21, 45, 99, 153, 207, 261, 315],
+    aspect_ratios = [[2, 0.5], [2, 0.5, 3, 1/3], [2, 0.5, 3, 1/3], [2, 0.5, 3, 1/3], [2, 0.5], [2, 0.5]],
+    pre_backbone = None,
+    middle_blocks = [{'kernel_size': (1,3), 'padding': (0,1), 'stride': (1,2)}, 
+                    {'kernel_size': (1,3), 'padding': (0,1), 'stride': (1,2)},
+                    {'kernel_size': (1,3), 'padding': (0,1), 'stride': (1,2)},
+                    {'kernel_size': (1,3), 'padding': (0,0), 'stride': (1,1)},
+                    {'kernel_size': (1,3), 'padding': (0,0), 'stride': (1,1)}],
+    head = {'num_defaults': [4, 6, 6, 6, 4, 4],
+            'loc': [{'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1}],
+            'conf': [{'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1}]
+            }
+)
+
+dataset = dict(
+    folders = ['10001_Urban_Clear_Noon'],
+    cameras = ['Cognata_Camera_01_8M']
+)
