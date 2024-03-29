f6fa60545abf241fb0bcea7654b31b4e581e3daa
Radoyeh Shojaei <radoyeh@gmail.com>
2024-03-29 16:15:36 -0700

    Add additional feature map config

---
+++ config/baseline_4MP_ss_fm1.py
@@ -0,0 +1,40 @@
+model = dict(
+    image_size = (1440, 2560),
+    feat_size = [(180, 320), (90, 160), (45, 80), (23, 40), (12, 20), (6, 10), (3, 5)],
+    steps = [8, 16, 32, 64, 128, 256, 512],
+    scales = [21, 45, 99, 153, 207, 261, 315, 450],
+    aspect_ratios = [[2, 0.5], [2, 0.5, 3, 1/3], [2, 0.5, 3, 1/3], [2, 0.5, 3, 1/3], [2, 0.5], [2, 0.5], [2, 0.5]],
+    pre_backbone = None,
+    feature_out_channels = [1024, 512, 512, 256, 256, 256, 256],
+    feature_in_channels = [256, 256, 128, 128, 128, 128],
+    backbone = [{'block_no': 6, 'layer': 0, 'conv': [{1: {'kernel_size': (1, 1), 'padding': (0,0), 'stride': (1, 1)},
+                                                      2: {'kernel_size': (3, 3), 'padding': (1,1), 'stride': (1, 1)}}],
+                                'downsample': {'kernel_size': (1, 1), 'padding': 0, 'stride': (1, 1)}}],
+    middle_blocks = [{'kernel_size': (1,3), 'padding': (0,1), 'stride': (1,2)}, 
+                    {'kernel_size': (1,3), 'padding': (0,1), 'stride': (1,2)},
+                    {'kernel_size': (1,3), 'padding': (0,1), 'stride': (1,2)},
+                    {'kernel_size': (1,3), 'padding': (0,1), 'stride': (1,2)},
+                    {'kernel_size': (1,3), 'padding': (0,1), 'stride': (1,2)},
+                    {'kernel_size': (1,3), 'padding': (0,1), 'stride': (1,2)}],
+    head = {'num_defaults': [4, 6, 6, 6, 4, 4, 4],
+            'loc': [{'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1}],
+            'conf': [{'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1},
+                    {'kernel_size': 3, 'padding': 1, 'stride': 1}]
+            }
+)
+
+dataset = dict(
+    folders = ['10001_Urban_Clear_Noon', '10002_Urban_Clear_Morning', '10003_Urban_Clear_Noon', '10004_Urban_Clear_Noon', '10005_Urban_Clear_Noon'],
+    cameras = ['Cognata_Camera_01_8M', 'Cognata_Camera_02_8M', 'Cognata_Camera_03_8M']
+)
